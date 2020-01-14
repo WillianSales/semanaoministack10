8b91e20616538ab39c036fc14a1f6f23695a61ef
Willian Sales <willian.o.sales@hotmail.com>
2020-01-14 23:25:10 +0000

    Created DevsController and SearchController
    Class 2 - 14/01/2020

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+// app. get, post, put, delete
+
+// Tipos de parametros: 
+// Query: request.query
+// Route: request.params
+// Body: request.body
+const app = express();
+
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@willian-m001-epyvg.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
