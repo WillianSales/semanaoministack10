f4479fa1c60a91e088174613724eaf36317e20e6
Willian Sales <willian.o.sales@hotmail.com>
2020-01-15 23:10:29 +0000

    Created front-end
    Created front-end and request api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 // app. get, post, put, delete
@@ -15,6 +16,7 @@ mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@willian-m001-ep
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
